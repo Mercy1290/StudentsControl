59d579961648bbbed35f7c766dfa4dbd4d9905c4
Mercy Triminio <mercycarolina@twinsys-hn.com>
2017-04-12 11:59:13 -0600

    Add project files.

---
+++ StudentsControl/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentsControl.MvcApplication" Language="C#" %>
---
+++ StudentsControl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentsControl.Startup))]
+namespace StudentsControl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
